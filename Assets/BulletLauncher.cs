1361cf18626cbb1d880241b4d38dbc6ecbd3c4fc
SaiNagaBala <saibala.k@xcubelabs.com>
2022-02-24 15:23:50 +0530

    bullet direction initialised

---
+++ Assets/BulletLauncher.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLauncher : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public int speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject tempBullet;
+            tempBullet = Instantiate(bulletPrefab,transform.position,Quaternion.identity);
+            tempBullet.GetComponent<Rigidbody>().velocity = Vector3.forward;
+        }
+    }
+}
